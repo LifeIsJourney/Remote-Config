c231104e85a16acb3af7a9735e6a0b375471849e
LifeIsJourney <44763493+LifeIsJourney@users.noreply.github.com>
2021-01-22 19:45:13 +0530

    Update popup

---
+++ Script/DotweenHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DotweenHelper : MonoBehaviour
+{
+    public bool scaleOnStart;
+    [SerializeField] float showTime=0.3f, hideTime=0.2f;
+    [SerializeField] Ease showEase=Ease.OutBack, hideEase=Ease.InBack;
+    [SerializeField] Transform tweenGo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        if (scaleOnStart)
+        {
+            if (tweenGo == null) { tweenGo = transform; }
+            tweenGo.localScale = Vector3.zero;
+            tweenGo.DOScale(1, showTime).SetEase(showEase);
+        }
+    }
+}
